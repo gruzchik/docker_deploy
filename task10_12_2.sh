7ad0a2a12657572518df193a9f8b7fc7a1f7fd23
Pro <i.pogorelko@gmail.com>
2018-05-10 23:05:54 +0300

    add docker-compose and nginx generate

--- task10_12_2.sh
+++ task10_12_2.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source ${SCRIPTPATH}/config
 
 # check if docker was installed
 if [ ! -x "$(command -v docker)" ] || [ ! -x "$(command -v docker-compose)" ]; then
@@ -11,3 +13,44 @@ if [ ! -x "$(command -v docker)" ] || [ ! -x "$(command -v docker-compose)" ]; t
 	add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 	apt-get update && apt-get install -y docker-ce docker-compose
 fi
+
+# generate nginx.conf file
+[ ! -d ${SCRIPTPATH}/etc ] && mkdir -p ${SCRIPTPATH}/etc
+cat <<EOF > ${SCRIPTPATH}/etc/nginx.conf
+worker_processes 1;
+#daemon off;
+#access_log /var/log/nginx/access.log;
+#error_log /var/log/nginx/error.log;
+events {
+    worker_connections 1024;
+}
+http {
+        server {
+            listen          80;
+            server_name     nginx;
+            location / {
+                proxy_pass  http://apache;
+            }
+        }
+}
+EOF
+
+# generate docker-compose file
+cat <<EOF > ${SCRIPTPATH}/docker-compose.yml
+version: '2'
+services:
+ nginx:
+  image: ${NGINX_IMAGE}
+  container_name: mir_nginx
+  ports:
+   - "${NGINX_PORT}:80"
+  volumes:
+   - ${SCRIPTPATH}/etc/nginx.conf:/etc/nginx/nginx.conf
+  links:
+   - apache
+ apache:
+  image: ${APACHE_IMAGE}
+  container_name: mir_apache
+EOF
+
+docker-compose up -d
