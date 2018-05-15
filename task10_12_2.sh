cb10510076180f96a1dbe24426fcd93244a9cec6
Pro <i.pogorelko@gmail.com>
2018-05-16 02:44:24 +0300

    add SSL sert

--- task10_12_2.sh
+++ task10_12_2.sh
@@ -14,6 +14,22 @@ if [ ! -x "$(command -v docker)" ] || [ ! -x "$(command -v docker-compose)" ]; t
 	apt-get update && apt-get install -y docker-ce docker-compose
 fi
 
+# generate root certificate
+[ ! -d ${SCRIPTPATH}/certs ] && mkdir -p ${SCRIPTPATH}/certs
+
+openssl genrsa -out ${SCRIPTPATH}/certs/root-ca.key 2048
+openssl req -x509 -days 365 -new -nodes -key ${SCRIPTPATH}/certs/root-ca.key -sha256 -out ${SCRIPTPATH}/certs/root-ca.crt -subj "/C=UA/ST=Kharkiv/L=Kharkiv/O=Nure/OU=Admin/CN=rootCA"
+# generate nginx cert
+openssl genrsa -out ${SCRIPTPATH}/certs/web.key 2048
+openssl req -nodes -new -sha256 -key ${SCRIPTPATH}/certs/web.key -out ${SCRIPTPATH}/certs/web.csr -subj "/C=UA/ST=Kharkiv/L=Kharkiv/O=Datacenter/OU=Server/CN=${HOST_NAME}"
+openssl x509 -req -extfile <(printf "subjectAltName=IP:${EXTERNAL_IP},DNS:www.${HOST_NAME}") -days 365 -in ${SCRIPTPATH}/certs/web.csr -CA ${SCRIPTPATH}/certs/root-ca.crt -CAkey ${SCRIPTPATH}/certs/root-ca.key -CAcreateserial -out ${SCRIPTPATH}/certs/web.crt
+
+# add SSL_CHAIN
+cat ${SCRIPTPATH}/certs/web.crt ${SCRIPTPATH}/certs/root-ca.crt > ${SCRIPTPATH}/certs/web-bundle.crt
+cp -aR ${SCRIPTPATH}/certs/* /etc/ssl/certs/
+SSL_KEY="/etc/ssl/certs/web.key"
+SSL_CHAIN="/etc/ssl/certs/web-bundle.crt"
+
 # generate nginx.conf file
 [ ! -d ${SCRIPTPATH}/etc ] && mkdir -p ${SCRIPTPATH}/etc
 cat <<EOF > ${SCRIPTPATH}/etc/nginx.conf
@@ -26,7 +42,11 @@ http {
         error_log /var/log/nginx/error.log;
         access_log /var/log/nginx/access.log;
         server {
-            listen          80;
+            #listen          80;
+	    listen 80 ssl;
+            ssl on;
+            ssl_certificate ${SSL_CHAIN};
+            ssl_certificate_key ${SSL_KEY};
             server_name     nginx;
             location / {
                 proxy_pass  http://apache;
@@ -48,6 +68,7 @@ services:
   volumes:
    - ${SCRIPTPATH}/etc/nginx.conf:/etc/nginx/nginx.conf
    - /srv/log/nginx:/var/log/nginx
+   - /etc/ssl/certs:/etc/ssl/certs
   links:
    - apache
  apache:
